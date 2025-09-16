5a2a8f9fd05b81fee460a897f4833975fcbba619
Caroline Rayfield <rayfielc@amazon.com>
2025-09-17 00:41:11 +0100

    Test automation - update to v2.2

--- speech_formatter.py
+++ speech_formatter.py
@@ -10,7 +10,7 @@ from streamlit_mic_recorder import mic_recorder
 
 # Configure Streamlit for load balancer path
 st.set_page_config(
-    page_title="Speech-to-Document Formatter v2.1",
+    page_title="Speech-to-Document Formatter v2.2",
     page_icon="🎤",
     layout="wide",
     initial_sidebar_state="expanded"
